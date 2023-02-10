4d40927437bfd8eac0650cab19edae3bba81c198
Zakaria Sabra <zakaria.sabra@gmail.com>
2023-02-10 13:00:29 +0100

    initial commit

---
+++ scrape.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup as BS
+import urllib.request
+import logging
+
+baseurl = "https://xn--frmnsvrde-02ah5r.se"
+
+html = urllib.request.urlopen(baseurl + "/formansvarde-bil")
+soup = BS(html, "html.parser")
+brands = soup.findAll('a', {'style': 'color:#303030'})
+
+print("Namn;Märke;Pris;Förmånsvärde;Url")
+
+for brand in brands:
+    html = urllib.request.urlopen(baseurl + brand["href"])
+    soup = BS(html, "html.parser")
+    cars = soup.findAll('a', {'style': 'margin-top:1rem;'})
+    for car in cars:
+        html = urllib.request.urlopen(baseurl + car["href"])
+        soup = BS(html, "html.parser")
+        variants = soup.findAll('td', {'class': 'variant-row'})
+        for variant in variants:
+            namnEl = variant.select("div.col-10.font-weight-bold > a")
+            priceEl = variant.select('.d-inline-block a')
+            förmånsEl = variant.findAll('div', {'class': 'row'})[2].find("a")
+            print(namnEl[0].text + ";" +brand.findAll("span", {"style": "font-size: 1.2em;font-weight: 600"})[0].text + ";"  + priceEl[0].text.strip() + ";" + förmånsEl.text.strip() + ";" + baseurl + namnEl[0]["href"])
+
