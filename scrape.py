70ba29ad5aece2d69d11182c5f0c94c8d3a68bf8
Zakaria Sabra <zakaria.sabra@gmail.com>
2023-02-10 13:58:48 +0100

    add engine

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ html = urllib.request.urlopen(baseurl + "/formansvarde-bil")
 soup = BS(html, "html.parser")
 brands = soup.findAll('a', {'style': 'color:#303030'})
 
-print("Namn;Märke;Pris;Förmånsvärde;Url")
+print("Namn;Märke;Typ;Pris;Förmånsvärde;Url")
 
 for brand in brands:
     html = urllib.request.urlopen(baseurl + brand["href"])
@@ -19,8 +19,10 @@ for brand in brands:
         soup = BS(html, "html.parser")
         variants = soup.findAll('td', {'class': 'variant-row'})
         for variant in variants:
+            engineEls = variant.select(".badge.badge-light")
+            engines = ",".join(list(map(lambda item: item.text, engineEls)))
             namnEl = variant.select("div.col-10.font-weight-bold > a")
             priceEl = variant.select('.d-inline-block a')
             förmånsEl = variant.findAll('div', {'class': 'row'})[2].find("a")
-            print(namnEl[0].text + ";" +brand.findAll("span", {"style": "font-size: 1.2em;font-weight: 600"})[0].text + ";"  + priceEl[0].text.strip() + ";" + förmånsEl.text.strip() + ";" + baseurl + namnEl[0]["href"])
+            print(namnEl[0].text + ";" + brand.findAll("span", {"style": "font-size: 1.2em;font-weight: 600"})[0].text + ";" + engines + ";" + priceEl[0].text.strip() + ";" + förmånsEl.text.strip() + ";" + baseurl + namnEl[0]["href"])
 
